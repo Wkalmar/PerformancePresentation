9c399f5b0beb804dd24add8bbb60d5f56e4eb4b2
Bohdan Stupak <stupakbogdan@ukr.net>
2022-06-01 17:57:51 +0300

    lazy evaluation benchmark

---
+++ Caching.LazyEvaluation.Benchmarking/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using Caching.LazyEvaluation;
+
+var summary = BenchmarkRunner.Run<CacheBenchmark>();
+Console.ReadLine();
+
+[MemoryDiagnoser]
+public class CacheBenchmark
+{
+    [Benchmark]
+    public void EstimateCacheMaxCache()
+    {
+        const int MaxOperations = 100000;
+        var fancy = new Fancy();
+        for (var i = 0; i < MaxOperations / 2; i++)
+        {
+            fancy.Append(100);
+        }
+        for (var i = 0; i < MaxOperations / 2; i++)
+        {
+            fancy.GetIndex(i);
+        }
+    }
+}
