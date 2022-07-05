c4fc7d4ff990dab73e0b24f3e10aababedf33a28
Bohdan Stupak <stupakbogdan@ukr.net>
2022-07-05 17:54:04 +0300

    remove redundant async

--- ByteArray.ToStream/Program.cs
+++ ByteArray.ToStream/Program.cs
@@ -13,14 +13,14 @@ public class HasingBenchmark
     private const string path = @"D:\Programming\batching\0000f821-a001-47e1-a56c-888ded5f6c99.wav";
     
     [Benchmark]
-    public async Task ByteArray()
+    public void ByteArray()
     {
         var bytes = File.ReadAllBytes(path);
         HashUtils.ComputeHash(bytes);
     }
 
     [Benchmark]
-    public async Task Stream()
+    public void Stream()
     {
         using var stream = File.OpenRead(path);
         HashUtils.ComputeStream(stream);
